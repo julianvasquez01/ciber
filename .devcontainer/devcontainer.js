9e13e5a5f30625698a24692f32443e7a1e0b081b
julianvasquez01 <julian.vasquez@usantoto.edu.co>
2026-04-08 08:30:19 -0500

    Create devcontainer.js

---
+++ .devcontainer/devcontainer.js
@@ -0,0 +1,11 @@
+{
+  "name": "Python DevContainer",
+  "image": "ghcr.io/devcontainers/templates/typescript-node:5.0.0",
+  "customizations": {
+    "vscode": {
+      "extensions":"esbenp.prettier-vscode" [
+        
+      ]
+    }
+  }
+}
